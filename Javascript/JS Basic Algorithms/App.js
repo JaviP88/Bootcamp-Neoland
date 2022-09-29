83fd7c865de268e20b64d1452f70e6fa57e9732e
Javi-P <javiperezpajuelo@gmail.com>
2022-09-29 20:09:04 +0200

    Iteration#1

---
+++ Javascript/JS Basic Algorithms/App.js
@@ -0,0 +1,7 @@
+/* --------------------------**Iteración #1: Variables**--------------------------*/
+
+// 1.1 Crea una variable llamada myFavoriteHero, asigna el valor Hulk a ella.
+
+const myFavouriteHero = 'Hi, my favourite hero is ' + 'Hulk' + '.'
+console.log(myFavouriteHero)
+

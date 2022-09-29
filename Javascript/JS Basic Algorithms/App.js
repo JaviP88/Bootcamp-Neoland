4ef57b9b5504417e297f0c0b7b6b0f7e4060853c
Javi-P <javiperezpajuelo@gmail.com>
2022-09-29 20:58:51 +0200

    Iteration#3

--- Javascript/JS Basic Algorithms/App.js
+++ Javascript/JS Basic Algorithms/App.js
@@ -22,3 +22,34 @@ console.log(y);
 let z = h + y;
 console.log(z);
 
+
+/*-------------------**Iteración #3: Operadores**-------------------------*/
+
+// 1.1 Multiplica 10 por 5 y muestra el resultado mediante console.
+
+let mul = 10 * 5;
+console.log(mul);
+
+// 1.2 Divide 10 por 2 y muestra el resultado en un console.
+
+let div = 10 / 2;
+console.log(div);
+
+// 1.3 Muestra mediante un console el resto de dividir 15 por 9.
+
+let divDec = 15 / 9;
+console.log(divDec);
+
+// 1.4 Usa el correcto operador de asignación que resultará en o = 15, teniendo dos variables p = 10 y j = 5.
+
+const p = 10;
+const j = 5;
+const o = p + j;
+console.log(o);
+
+// 1.5 Usa el correcto operador de asignación que resultará en i = 50, teniendo dos variables c = 10 y m = 5.
+
+const c = 10;
+const m = 5;
+const i = c * m;
+console.log(i);

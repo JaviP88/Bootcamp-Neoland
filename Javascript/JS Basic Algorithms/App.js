840add5586cc58fb4210aa5fd76dc4e8478dc2a5
Javi-P <javiperezpajuelo@gmail.com>
2022-09-29 20:47:00 +0200

    Iteration#1Done

--- Javascript/JS Basic Algorithms/App.js
+++ Javascript/JS Basic Algorithms/App.js
@@ -2,6 +2,23 @@
 
 // 1.1 Crea una variable llamada myFavoriteHero, asigna el valor Hulk a ella.
 
-const myFavouriteHero = 'Hi, my favourite hero is ' + 'Hulk' + '.'
-console.log(myFavouriteHero)
+const myFavouriteHero = 'Hi, my favourite hero is ' + 'Hulk' + '.';
+console.log(myFavouriteHero);
+
+// 1.2 Crea una variable llamada x, asigna el valor 50 a ella.
+
+let x = 50;
+console.log(x);
+
+// 1.3 Crea una variable llamada 'h' con el valor 5 y otra 'y' con el valor 10. 
+
+let h = 5;
+let y = 10;
+console.log(h);
+console.log(y);
+
+// 1.4 Crea una otra variable 'z' y asignale el valor de 'h' + 'y'.
+
+let z = h + y;
+console.log(z);
 

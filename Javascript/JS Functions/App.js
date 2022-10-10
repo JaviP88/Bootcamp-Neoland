67a5e1e63edc3029bf35fac355211d349a9cdd9b
Javi-P <javiperezpajuelo@gmail.com>
2022-10-10 13:47:12 +0200

    Iteration#3

--- Javascript/JS Functions/App.js
+++ Javascript/JS Functions/App.js
@@ -27,7 +27,6 @@ console.log(result2);
 console.log(result3);
 
 
-
 /*---------------**Iteración #2: Buscar la palabra más larga**---------------
 
 Completa la función que tomando un array de strings como argumento devuelva el más largo, en caso de que dos strings tenga la misma longitud deberá devolver el primero.
@@ -57,4 +56,33 @@ let palabraMasLarga = theLargestWord(avengers);    // Crea una nueva variable qu
 console.log(palabraMasLarga);                      // Pinta lo que devuelve en pantalla.
 
 console.log('-----Solución con arrow...');
+
+
+/*---------------**Iteración #3: Calcular la suma**-------------------
+
+Calcular una suma puede ser tan simple como iterar sobre un array y sumar cada uno de los elementos.
+Implemente la función denominada sumNumbers que toma un array de números como argumento y devuelve la suma de todos los números de la matriz. 
+
+Puedes usar este array para probar tu función:
+
+const numbers = [1, 2, 3, 5, 45, 37, 58];
+
+function sumAll(param) {
+  // insert code
+}*/
+console.log('---------------**Iteración #3: Calcular la suma**---------------');
+
+const numbers = [1, 2, 3, 5, 45, 37, 58];
+
+function sumAll(param){
+  let acumulator = 0;                     // Crear un acumulador tipo number
+  for(i = 0; i < param.length; i++){      // Lo que se itera es param, NO numbers
+  acumulator += param[i];                 // Esto es igual a ==> acumulator = acumulator + param[i];
+  }
+return acumulator;
+}
+
+let total = sumAll(numbers);
+console.log(total);
+
 

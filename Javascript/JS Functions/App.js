2968958d33cd7fe948ca5e0814c0ca2bf11a5394
Javi-P <javiperezpajuelo@gmail.com>
2022-10-13 21:28:37 +0200

    Iteration#7

--- Javascript/JS Functions/App.js
+++ Javascript/JS Functions/App.js
@@ -219,3 +219,52 @@ let newRemoveDuplicatesArrow = removeDuplicatesArrow(duplicates);
 console.log(newRemoveDuplicates);
 
 
+/*--------------**Iteración #7: Buscador de nombres**-----------------------
+
+Crea una función que reciba por parámetro un array y el valor que desea comprobar que existe dentro de dicho array - comprueba si existe el elemento, en caso que existan nos devuelve un true y la posición de dicho elemento y por la contra un false. Puedes usar este array para probar tu función:
+
+const nameFinder = [
+  'Peter',
+  'Steve',
+  'Tony',
+  'Natasha',
+  'Clint',
+  'Logan',
+  'Xabier',
+  'Bruce',
+  'Peggy',
+  'Jessica',
+  'Marc'
+];
+function finderName(param) {
+  // insert code
+}*/
+
+console.log('---------------**Iteración #7: Buscador de nombres**---------------');
+
+const nameFinder = [
+  'Peter',
+  'Steve',
+  'Tony',
+  'Natasha',
+  'Clint',
+  'Logan',
+  'Xabier',
+  'Bruce',
+  'Peggy',
+  'Jessica',
+  'Marc'
+];
+
+const finderName = (array, palabra) => {
+  let nameIndexArray = []
+  array.forEach((name, i) =>{
+    if(name === palabra){
+      nameIndexArray.push(`${name} ${i}`)
+    }
+    
+  })
+  console.log(nameIndexArray);
+}
+finderName(nameFinder, 'Marc');
+

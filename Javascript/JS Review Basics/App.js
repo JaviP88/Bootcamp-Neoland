c228bddfcc92a80f9b1bf69d097b3bc298afe6d1
Javi-P <javiperezpajuelo@gmail.com>
2022-10-20 20:39:03 +0200

    Iteration#5

--- Javascript/JS Review Basics/App.js
+++ Javascript/JS Review Basics/App.js
@@ -207,16 +207,27 @@ let findIndexSportsQuidich =findArrayIndex2(sports, 'Quidich');
 console.log(findIndexSportsQuidich);
 
 
-/***Iteración #5: Función rollDice**
+//------------**Iteración #5: Función rollDice**------------
+console.log('------------**Iteración #5: Función rollDice**------------')
 
-Crea una función llamada **rollDice()** que reciba como parametro el numero de caras que queramos que tenga el dado que deberá silumar el codigo dentro de la función. Como hemos dicho, que la función use el parametro para simular una tirada de dado y retornar el resultado. Si no se te ocurre como hacer un numero aleatorio no te preocupes! busca información sobre la función de javascript **Math.random();**
+/*Crea una función llamada **rollDice()** que reciba como parametro el numero de
+caras que queramos que tenga el dado que deberá simular el codigo dentro de la
+función. Como hemos dicho, que la función use el parametro para simular una
+tirada de dado y retornar el resultado. Si no se te ocurre como hacer un numero
+aleatorio no te preocupes! busca información sobre la función de javascript **Math.random();** */
 
+const rollDice = (numberFaces) => {
+    return Math.floor(Math.random() * (numberFaces - 0 + 1) + 0);   //operar con 0 es por seguir la fórmula, ya que el mínimo de mi dado es 0
+}
 
+console.log(rollDice(56));
 
+/***Iteración #6: Función swap**
 
-**Iteración #6: Función swap**
-
-Crea una función llamada `swap()` que reciba un array y dos parametros que sean indices del array. La función deberá intercambiar la posición de los valores de los indices que hayamos enviado como parametro. Retorna el array resultante.
+Crea una función llamada `swap()` que reciba un array y dos parametros que sean indices del
+array. La función deberá intercambiar la posición de los valores de los indices que hayamos
+enviado como parametro. Retorna el array resultante.
 
 Sugerencia de array:
 ['Mesirve', 'Cristiano Romualdo', 'Fernando Muralla', 'Ronalguiño']*/
+

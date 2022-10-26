5b352eac7ecfe248523ab9bfa22c95ba16db5eed
Javi-P <javiperezpajuelo@gmail.com>
2022-10-26 20:01:36 +0200

    Iteration#3

--- Javascript/JS ES6/App.js
+++ Javascript/JS ES6/App.js
@@ -37,8 +37,6 @@ console.log(gender);
 console.log(year);
 
 
-
-
 /*2.2 En base al siguiente javascript, usa destructuring para crear 3 variables 
 llamadas fruit1, fruit2 y fruit3, con los valores del array. Posteriormente
 imprimelo por consola.
@@ -67,6 +65,7 @@ const {name, race} = animalFunction();
 console.log(name);
 console.log(race);
 
+
 /*2.4 En base al siguiente javascript, usa destructuring para crear las 
 variables name y itv con sus respectivos valores. Posteriormente crea 
 3 variables usando igualmente el destructuring para cada uno de los años 
@@ -84,30 +83,58 @@ console.log(itv1);
 console.log(itv2);
 console.log(itv3);
 
+
+
 //-----------**Iteración #3: Spread Operator**--------------
 console.log('-----------**Iteración #3: Spread Operator**--------------');
  /*3.1 Dado el siguiente array, crea una copia usando spread operators.
-const pointsList = [32, 54, 21, 64, 75, 43]
+const pointsList = [32, 54, 21, 64, 75, 43]*/
+const pointsList = [32, 54, 21, 64, 75, 43];
 
-3.2 Dado el siguiente objeto, crea una copia usando spread operators.
+const pointListCopy = [...pointsList];
+console.log(pointListCopy);
+
+
+/*3.2 Dado el siguiente objeto, crea una copia usando spread operators.
+const toy = {name: 'Bus laiyiar', date: '20-30-1995', color: 'multicolor'};*/
 const toy = {name: 'Bus laiyiar', date: '20-30-1995', color: 'multicolor'};
 
-3.3 Dado los siguientes arrays, crea un nuevo array juntandolos usando 
+const toyCopy = {...toy};
+console.log(toyCopy);
+
+
+/*3.3 Dado los siguientes arrays, crea un nuevo array juntandolos usando 
 spread operatos.
-const pointsList = [32, 54, 21, 64, 75, 43];
+const pointList = [32, 54, 21, 64, 75, 43];
+const pointsLis2 = [54,87,99,65,32];*/
+const pointList = [32, 54, 21, 64, 75, 43];
 const pointsLis2 = [54,87,99,65,32];
 
-3.4 Dado los siguientes objetos. Crea un nuevo objeto fusionando los dos 
+const newPointsList = [...pointsList, ...pointsLis2];
+console.log(newPointsList);
+
+
+/*3.4 Dado los siguientes objetos. Crea un nuevo objeto fusionando los dos 
 con spread operators.
 const toy = {name: 'Bus laiyiar', date: '20-30-1995', color: 'multicolor'};
+const toyUpdate = {lights: 'rgb', power: ['Volar like a dragon', 'MoonWalk']}*/
+const toyyy = {name: 'Bus laiyiar', date: '20-30-1995', color: 'multicolor'};
 const toyUpdate = {lights: 'rgb', power: ['Volar like a dragon', 'MoonWalk']}
 
-3.5 Dado el siguiente array. Crear una copia de él eliminando la posición 2 
-pero sin editar el array inicial. De nuevo, usando spread operatos.
-const colors = ['rojo', 'azul', 'amarillo', 'verde', 'naranja'];
-```
+const newToysList = {...toyyy, ...toyUpdate};
+console.log(newToysList);
 
-**Iteración #4: Map**
+/*3.5 Dado el siguiente array. Crear una copia de él eliminando la posición 2 
+pero sin editar el array inicial. De nuevo, usando spread operatos.
+const colors = ['rojo', 'azul', 'amarillo', 'verde', 'naranja'];*/
+const colors = ['rojo', 'azul', 'amarillo', 'verde', 'naranja'];
+
+let colorsCopy = [...colors];
+colorsCopy.splice(2, 1);
+console.log(colorsCopy);
+
+
+/***Iteración #4: Map**
 
 ```jsx
 4.1 Dado el siguiente array, devuelve un array con sus nombres 

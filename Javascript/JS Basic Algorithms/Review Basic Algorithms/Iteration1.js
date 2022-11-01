7325ac5bb1a34d7a1f0811ef72ee2706eb1f8bc2
Javi-P <javiperezpajuelo@gmail.com>
2022-11-01 14:53:15 +0100

    Remake#iteration#1

---
+++ Javascript/JS Basic Algorithms/Review Basic Algorithms/Iteration1.js
@@ -0,0 +1,23 @@
+/* --------------------------**Iteración #1: Variables**--------------------------*/
+
+// 1.1 Crea una variable llamada myFavoriteHero, asigna el valor Hulk a ella.
+
+const myFavouriteHero = 'Hulk';
+console.log(myFavouriteHero);
+
+// 1.2 Crea una variable llamada x, asigna el valor 50 a ella.
+
+const x = 50;
+console.log(`La bariable 'x' vale: ${x}`);
+
+// 1.3 Crea una variable llamada 'h' con el valor 5 y otra 'y' con el valor 10. 
+
+const h = 5;
+const y = 10;
+console.log(`La bariable 'h' vale: ${h}`);
+console.log(`La bariable 'y' vale: ${y}`);
+
+// 1.4 Crea una otra variable 'z' y asignale el valor de 'h' + 'y'.
+
+const z = h + y;
+console.log(`El valor de 'z' es igual a ${z}, que es la suma de 'h' e 'y'`);

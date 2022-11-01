aced2be2031b1e3394fa8f0b7544df19ffd2908d
Javi-P <javiperezpajuelo@gmail.com>
2022-11-01 14:39:43 +0100

    Preparado ejercicios para rehacer

---
+++ Javascript/JS Basic Algorithms/Review Basic Algorithms/Iteration 6.js
@@ -0,0 +1,14 @@
+/*---------------------**Iteración #6: Bucles**--------------------------*/
+
+//6.1 Crea un bucle for que vaya desde 0 a 9 y muestralo por consola.
+
+
+/*6.2 Crea un bucle for que vaya desde 0 a 9 y muestralo por consola solo 
+cuando el resto del numero dividido entre 2 sea 0.*/
+
+
+
+/*6.3 Crea un bucle para conseguir dormir contando ovejas. 
+Este bucle tiene que dar 10 vueltas, es decir, 10 console.log.
+Muestra por consola un mensaje diciendo 'Intentando dormir 🐑' en cada vuelta del bucle 
+y cambia el mensaje en la décima vuelta a 'Dormido!'.*/

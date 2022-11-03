be7c5c1c3e8e0245e9aec8f9d3a77758a3012789
Javi-P <javiperezpajuelo@gmail.com>
2022-11-03 20:09:39 +0100

    Remake#iteration#3

--- Javascript/JS Basic Algorithms/Review Basic Algorithms/Iteration3.js
+++ Javascript/JS Basic Algorithms/Review Basic Algorithms/Iteration3.js
@@ -2,18 +2,29 @@
 
 // 3.1 Multiplica 10 por 5 y muestra el resultado mediante console.
 
-
+const mul = 10 * 5;
+console.log(mul);
 
 // 3.2 Divide 10 por 2 y muestra el resultado en un console.
 
-
+const div = 10 / 2;
+console.log(div);
 
 // 3.3 Muestra mediante un console el resto de dividir 15 por 9.
 
-
+const resto = 15 % 9;
+console.log(resto);
 
 // 3.4 Usa el correcto operador de asignación que resultará en o = 15, teniendo dos variables p = 10 y j = 5.
 
-
+const p = 10;
+const j = 5;
+const o = p + j;
+console.log(o);
 
 // 3.5 Usa el correcto operador de asignación que resultará en i = 50, teniendo dos variables c = 10 y m = 5.
+
+const c = 10;
+const m = 5;
+const i = 10 * 5;
+console.log(i);

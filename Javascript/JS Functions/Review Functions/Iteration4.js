a4d5cc94c187c3874f2b628dc1c5118484acccae
Javi-P <javiperezpajuelo@gmail.com>
2022-11-04 20:52:09 +0100

    Remake#iteration#4

---
+++ Javascript/JS Functions/Review Functions/Iteration4.js
@@ -0,0 +1,19 @@
+/*--------------**Iteración #4: Calcular el promedio**------------------
+
+Calcular un promedio es una tarea extremadamente común. Puedes usar este array para probar
+tu función:
+
+const numbers = [12, 21, 38, 5, 45, 37, 6];
+function average(param) {
+  // insert code
+}*/
+const numbers = [12, 21, 38, 5, 45, 37, 6];
+function average(param) {
+  let counter = 0;
+  for (item of param){
+    counter += item / 2;
+  };
+  console.log(counter);
+};
+average(numbers);
+

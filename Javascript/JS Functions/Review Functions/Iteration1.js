c9714b46c304e111a4c11d450ac517908f4e9713
Javi-P <javiperezpajuelo@gmail.com>
2022-11-04 18:41:16 +0100

    Remake#iteration#1

---
+++ Javascript/JS Functions/Review Functions/Iteration1.js
@@ -0,0 +1,17 @@
+/*---------------**Iteración #1: Buscar el máximo**-----------------
+
+Completa la función que tomando dos números como argumento devuelva el más alto.
+
+function sum(numberOne , numberTwo) {
+  // insert code
+}*/
+function sum (numberOne, numberTwo){
+  if (numberOne > numberTwo){
+    console.log(`El número más alto es: ${numberOne}`);
+  } else if (numberOne < numberTwo){
+    console.log(`El número más alto es ${numberTwo}`);
+  } else (console.log('Los dos números son iguales'));
+};
+sum (6, 4);
+sum (2, 8);
+sum (5, 5);

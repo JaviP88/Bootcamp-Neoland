1ef9da1256a8bc09b4466c225d79b6fd0139593f
Javi-P <javiperezpajuelo@gmail.com>
2022-11-04 21:09:57 +0100

    Remake#iteration#5

---
+++ Javascript/JS Functions/Review Functions/Iteration5.js
@@ -0,0 +1,22 @@
+/*--------------**Iteración #5: Calcular promedio de strings**----------------
+
+Crea una función que reciba por parámetro un array y cuando es un valor number lo sume y de lo
+contrario cuente la longitud del string y lo sume. Puedes usar este array para probar tu función:
+
+const mixedElements = [6, 1, 'Rayo', 1, 'vallecano', '10', 'upgrade', 8, 'hub'];
+function averageWord(param) {
+  // insert code
+}*/
+const mixedElements = [6, 1, 'Rayo', 1, 'vallecano', '10', 'upgrade', 8, 'hub'];
+function averageWord(param) {
+  let counter = 0;
+  for (item of param){
+    if (typeof(item) == 'number'){
+      counter += item;
+    } else if (typeof(item) == 'string'){
+      counter += item.length;
+    };
+  };
+  console.log(counter);
+};
+averageWord(mixedElements);

bf6d96ae1324b7dea404e967687f6b094b61632d
Javi-P <javiperezpajuelo@gmail.com>
2022-11-24 21:13:08 +0100

    Remake#iteration#6

--- Javascript/JS Review Basics/Review Review Basics/Iteration6.js
+++ Javascript/JS Review Basics/Review Review Basics/Iteration6.js
@@ -7,3 +7,15 @@ Sugerencia de array:
 ['Mesirve', 'Cristiano Romualdo', 'Fernando Muralla', 'Ronalguiño']*/
 
 const furgolPrayers = ['Mesirve', 'Cristiano Romualdo', 'Fernando Muralla', 'Ronalguiño'];
+
+function swap(array, prayer1, prayer2) {
+    const swap1 = array.indexOf(prayer1);
+    const swap2 = array.indexOf(prayer2);
+
+    array.splice(swap1, 1, prayer2);
+    array.splice(swap2, 1, prayer1);
+    console.log(array);
+};
+
+swap(furgolPrayers, 'Mesirve', 'Ronalguiño');
+

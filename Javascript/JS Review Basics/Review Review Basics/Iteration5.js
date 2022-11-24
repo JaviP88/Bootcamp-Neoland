a03e2fc00e1bde23601f024252e57c664390ec7e
Javi-P <javiperezpajuelo@gmail.com>
2022-11-24 21:22:59 +0100

    Remake#iteration#5

--- Javascript/JS Review Basics/Review Review Basics/Iteration5.js
+++ Javascript/JS Review Basics/Review Review Basics/Iteration5.js
@@ -7,3 +7,8 @@ función. Como hemos dicho, que la función use el parametro para simular una
 tirada de dado y retornar el resultado. Si no se te ocurre como hacer un numero
 aleatorio no te preocupes! busca información sobre la función de javascript **Math.random();** */
 
+const rollDice = (numberFaces) => {
+    return Math.floor(Math.random() * (numberFaces - 0 + 1) + 0);   //operar con 0 es por seguir la fórmula, ya que el mínimo de mi dado es 0
+}
+
+console.log(rollDice(3));

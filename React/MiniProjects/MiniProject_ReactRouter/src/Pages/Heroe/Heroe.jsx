0f41faedac14d33efef3e39e645e1d76fd9dd6c5
Javi-P <javiperezpajuelo@gmail.com>
2023-02-03 19:33:29 +0100

    Empezando mini-ejercicio React Router

---
+++ React/MiniProjects/MiniProject_ReactRouter/src/Pages/Heroe/Heroe.jsx
@@ -0,0 +1,10 @@
+const Heroe = () => {
+
+    return (
+        <div>
+            <h1>Heroe Component</h1>
+        </div>
+    )
+}
+
+export default Heroe
---
+++ React/MiniProjects/MiniProject_ReactRouter/src/Pages/Heroes/Heroes.jsx
@@ -0,0 +1,10 @@
+const Heroes = () => {
+
+    return (
+        <div>
+            <h1>Heroes Component</h1>
+        </div>
+    )
+}
+
+export default Heroes

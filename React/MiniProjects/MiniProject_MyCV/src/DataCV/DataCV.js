c6c7f8556e9175c4a15ed990dcaba0d37c2f4b4f
Javi-P <javiperezpajuelo@gmail.com>
2023-02-04 22:42:46 +0100

    DataCV: creado experience y education

---
+++ React/MiniProjects/MiniProject_MyCV/src/DataCV/DataCV.js
@@ -0,0 +1,177 @@
+export const DataCV = {
+    hero: {
+      name: "Javier",
+      surname: "Pérez Pajuelo",
+      city: "Madrid",
+      email: "javiperezpajuelo@gmail.com",
+      birthDate: "11/02/1988",
+      phone: "Yeeeah",
+      image: "https://i.imgur.com/ZQAkED3.png",
+      gitHub: "https://github.com/tonystark",
+      aboutMe: [
+        {
+          info: "🤖 My armor, it was never a distraction or a hobby, it was a cocoon. I am Iron Man.",
+        },
+        {
+          info: "🔩 CEO of Stark Industries.",
+        },
+        {
+          info: "🕶 Genius, billionaire, playboy, philanthropist.",
+        },
+        {
+          info: "🦾 I do have a responsibility to keep my inventions from evil hands – but I have a greater responsibility to oppose that evil any way I can.",
+        },
+      ],
+    },
+    education: [
+        {
+            name: "Bootcamp Full Stack Web Developer",
+            date: "En proceso",
+            where: "Neoland, Madrid.",
+        },  
+        {
+            name: "Máster de Profesorado de Educación Secundaria",
+            date: "2020 - 2021",
+            where: "Valencian International University.",
+        },
+        {
+            name: "Licenciado en Ciencias de la Actividad Física y del Deporte",
+            date: "2010 - 2012",
+            where: "Universidad de Granada.",
+        },
+        {
+            name: "Maestro especializado en Educación Física",
+            date: "2008 - 2010",
+            where: "Universidad de Huelva.",
+        },
+        {
+            name: "Técnico Superior en Animación de Actividades Físicas y Deportivas",
+            date: "2006 - 2008",
+            where: "I.E.S. Carmen Laffón, Sevilla.",
+        },
+    ],
+    experience: [
+        {
+            name: "Profesor de Educación Física de Secundaria (sustitución)",
+            date: "11/01/2023 - Actualmente",
+            where: "I.E.S. Francisco de Quevedo, Madrid.",
+            description:
+            "Profesor de la asignatura de Educación Física en E.S.O y Bachillerato.",
+        },
+        {
+            name: "Profesor de Educación Física de Secundaria (sustitución)",
+            date: "21/10/2022 - 09/12/2022",
+            where: "I.E.S. Josefina Aldecoa, Alcorcón, Madrid.",
+            description:
+            "Profesor de la asignatura de Educación Física en E.S.O y Bachillerato.",
+        },
+        {
+            name: "Padre de día",
+            date: "10/03/2021 - 30/06/2021",
+            where: "El nido de Muni, Majadahonda, Madrid.",
+            description:
+            "Cuidador de menores de 3 años, en un ambiente familiar.",
+        },
+        {
+            name: "Maestro de Educación Física de Primaria (refuerzo COVID)",
+            date: "14/09/2020 - 18/08/2020",
+            where: "Colegio Antamira, Paracuellos del Jarama, Madrid.",
+            description:
+            "Maestro de la asignatura de Educación Física en Primaria.",
+        },
+        {
+            name: "Maestro de Educación Física de Primaria (sustitución)",
+            date: "09/03/2020 - 13/03/2020",
+            where: "Colegio Joyfe, Madrid.",
+            description:
+            "Maestro de la asignatura de Educación Física en Primaria.",
+        },
+        {
+            name: "Vendedor",
+            date: "08/10/2019 - 15/06/2020",
+            where: "Decathlon, Majadahonda, Madrid",
+            description:
+            "Vendedor de material deportivo en la sección de ciclismo y roller.",
+        },
+        {
+            name: "Maestro de Educación Física de Infantil y Primaria",
+            date: "01/09/2017 - 30/06/2019",
+            where: "Escuela IDEO, Alcobendas, Madrid.",
+            description:
+            "Maestro de la asignatura de Educación Física en Infantil y Primaria.",
+        },
+        {
+            name: "Director técnico y tutor laboral",
+            date: "26/05/2015 - 30/12/2016",
+            where: "Over Limit Aventura S.L., Sevilla.",
+            description:
+            "Encargado de la gestión y coordinación de los monitores, de la planificación y creación de actividades y formador y tutor laboral de futuros preofesionales del turismo activo.",
+        },
+        {
+            name: "Profesor de esquí",
+            date: "01/11/2014 - 28/02/2015",
+            where: "VASS Canada, Vancouver.",
+            description:
+            "Profesor de esquí de niños y adultos.",
+        },
+        {
+            name: "Coordinador de equipos y monitor especialista",
+            date: "15/05/2010 - 15/02/2014",
+            where: "Over Limit Aventura S.L., Sevilla.",
+            description:
+            "Encargado de la coordicanción del equipo y planificación de las actividades, además de monitor especialista en escalada y piragüismo.",
+        },
+        {
+            name: "Maestro en prácticas de Educación Física",
+            date: "01/03/2010 - 30/04/2010",
+            where: "C.E.I.P. Virgen del Pilar, Huelva.",
+            description:
+            "Prácticas Universitarias como maestro de la especialidad de Educación Física en Primaria.",
+        },
+        {
+            name: "Maestro en prácticas en Primaria",
+            date: "01/03/2009 - 30/04/2009",
+            where: "C.E.I.P. García Lorca, Huelva.",
+            description:
+            "Prácticas Universitarias como tutor de 2º de Primaria.",
+        },
+        {
+            name: "Monitor de grupos y animador",
+            date: "20/06/2008 - 14/05/2010",
+            where: "Over Limit Aventura S.L., Sevilla.",
+            description:
+            "Encargado de la coordicanción del equipo y planificación de las actividades, además de monitor especialista en escalada y piragüismo.",
+        },
+        
+    ],
+    languages: {
+      language: "English",
+      wrlevel: "Native",
+      splevel: "Native",
+    },
+    habilities: [
+      "Robotics",
+      "Robot Programming",
+      "Physics",
+      "Weaponery",
+      "Engineer",
+      "Money",
+      "Dating",
+      "Saving the world",
+    ],
+    volunteer: [
+        {
+            name: "Profesor de esquí de niños/as con diversidad funcional",
+            date: "01/11/2014 - 28/02/2015",
+            where: "VASS Canada, Vancouver.",
+            description:
+            "Profesor de esquí de niños con deficiencia visual, auditiva, movilidad reducida, etc.",
+        },
+      {
+        name: "Damage Control",
+        where: "U.S.A.",
+        description:
+          "The United States Department of Damage Control, occasionally known as the DODC, is a department of the United States of America. Initially a subsidiary of S.H.I.E.L.D., Damage Control was an organization specializing in post-battle clean-up. Following the Battle of New York, Damage Control was made into an executive branch of the United States government, and in a joint venture with Stark Industries, was tasked with acquiring alien and other dangerous artifacts along with cleaning up damages caused by enhanced individuals.",
+      },
+    ],
+  };
